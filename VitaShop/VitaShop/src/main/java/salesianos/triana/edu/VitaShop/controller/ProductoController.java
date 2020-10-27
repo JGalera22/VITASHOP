d1f29e0016c9f7e053b1e5f1420f2a0effd40eeb
Javier García-Galera Rodríguez <galera221412@gmail.com>
2020-10-27 14:44:25 +0100

    impl carrito añadido, falta html

---
+++ VitaShop/VitaShop/src/main/java/salesianos/triana/edu/VitaShop/controller/ProductoController.java
@@ -0,0 +1,80 @@
+package salesianos.triana.edu.VitaShop.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import salesianos.triana.edu.VitaShop.repositorio.Carrito;
+import salesianos.triana.edu.VitaShop.seguridad.modelos.Producto;
+import salesianos.triana.edu.VitaShop.servicios.ProductoServicio;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ProductoController {
+
+        private final ProductoServicio productoServicio;
+        private final Carrito carrito;
+
+        @ModelAttribute("productos")
+        public List<Producto> todosLosProductos() {
+            return productoServicio.findAll();
+        }
+
+        @GetMapping({"/", "/list"})
+        public String index(Model model) {
+            return "index";
+        }
+
+
+        /**
+         * Métodos asociados al carrito
+         */
+        @GetMapping("/carrito/add/{id}")
+        public String addToCart(@PathVariable("id") Long id) {
+            carrito.addToCarrito(id);
+            return "redirect:/carrito/mostrar";
+        }
+
+        @GetMapping("/carrito/mostrar")
+        public String showCart(Model model) {
+            model.addAttribute("carrito", carrito.getCarrito());
+            model.addAttribute("numProductosDiferentes", carrito.numeroTotalProductosDiferentes());
+            model.addAttribute("importeTotal", carrito.importeTotal());
+            model.addAttribute("numUnidades", carrito.numeroTotalDeUnidades());
+            return "carrito";
+        }
+
+        @GetMapping("/carrito/remove/{id}")
+        public String removeFromCart(@PathVariable("id") Long id) {
+            carrito.removeFromCarrito(id);
+            return "redirect:/carrito/mostrar";
+        }
+
+        @GetMapping("/carrito/clear")
+        public String clearCart() {
+            carrito.clear();
+            return "redirect:/";
+        }
+
+
+        @GetMapping("/carrito/process")
+        public String procesarCarrito() {
+
+            // En este método habría que conectar con otro servicio
+            // para transformar los datos del carrito en una compra de
+            // verdad.
+
+            // TODO Procesamiento de productos
+
+            // Posteriormente, habría que vaciarlo y redirigir al usuario donde corresponda
+            carrito.clear();
+            return "redirect:/";
+
+        }
+
+}
